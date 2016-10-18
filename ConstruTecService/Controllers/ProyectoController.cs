62b89df7d49942035921ea44214d96473b02db6f
JuanPablo <juanp1995@gmail.com>
2016-10-17 23:47:53 -0600

    Método para registrar un proyecto

---
+++ ConstruTecService/Controllers/ProyectoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using ConstruTecService.Models;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace ConstruTecService.Controllers
+{
+    [RoutePrefix("projects")]
+    public class ProyectoController : ApiController
+    {
+
+        /// <summary>
+        /// Método que permite registrar un nuevo proyecto en la base de datos
+        /// </summary>
+        /// <param name="pProject"></param>
+        /// <returns>Id con el que se registro el proyecto</returns>
+        [Route("register")]
+        [HttpPost]
+        public IHttpActionResult register(Proyecto pProject)
+        {
+            using (NpgsqlConnection connection = DataBase.getConnection())
+            {
+                NpgsqlCommand command = new NpgsqlCommand("registrarproyecto", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pnombre", NpgsqlDbType.Text).Value = pProject._nombre;
+                command.Parameters.AddWithValue("@pprovincia", NpgsqlDbType.Text).Value = pProject._provincia;
+                command.Parameters.AddWithValue("@pcanton", NpgsqlDbType.Text).Value = pProject._canton;
+                command.Parameters.AddWithValue("@pdistrito", NpgsqlDbType.Text).Value = pProject._distrito;
+                command.Parameters.AddWithValue("@pcliente", NpgsqlDbType.Text).Value = pProject._cliente;
+                command.Parameters.AddWithValue("@pingeniero", NpgsqlDbType.Text).Value = pProject._ingeniero;
+
+                try
+                {
+                    connection.Open();
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+
+                    return Json(new Response(reader.GetString(0)));
+                }
+                catch (NpgsqlException ex) { return Json(new Response(Constants.ERROR_DATABASE_CONNECTION)); }
+                finally { connection.Close(); }
+            }
+        }
+
+
+
+    }
+}
---
+++ ConstruTecService/Models/Proyecto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConstruTecService.Models
+{
+
+    /// <summary>
+    /// Clase representa a la table PROYECTO en la base de datos PostgreSQL
+    /// </summary>
+    public class Proyecto
+    {
+
+        public int _id { get; set; }
+        public string _nombre { get; set; }
+        public bool _estado { get; set; }
+        public Decimal _costo { get; set; }
+        public string _provincia { get; set; }
+        public string _canton { get; set; }
+        public string _distrito { get; set; }
+        public string _cliente { get; set; }
+        public string _ingeniero { get; set; }
+
+    }
+}
